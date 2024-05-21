e8289f9053b060025b5d47d4899ed73bd54e427a
kk <karthikkrishna230104@gmail.com>
2024-05-21 11:48:29 +0530

    add

--- admin/src/app/login/login.component.ts
+++ admin/src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
@@ -11,10 +12,10 @@ export class LoginComponent {
   password: string = '';
   errorMessage: string = '';
 
-  constructor(private router: Router) {}
+  constructor(private authService: AuthService, private router: Router) {}
 
   login(): void {
-    if (this.username === 'admin' && this.password === 'admin') {
+    if (this.authService.login(this.username, this.password)) {
       this.router.navigate(['/']);
     } else {
       this.errorMessage = 'Invalid username or password';
